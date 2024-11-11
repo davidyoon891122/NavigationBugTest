643899d86e2b0329f107c877edf1666745beffba
David Yoon <qqpo12@naver.com>
2024-11-11 22:20:29 +0900

    Initial Commit

---
+++ NavigationBugTest/NavigationBugTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationBugTestApp.swift
+//  NavigationBugTest
+//
+//  Created by Jiwon Yoon on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationBugTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
